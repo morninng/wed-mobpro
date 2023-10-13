3abd0d2c437579c3fbd12fd0ac10c07fcb41a4e4
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-10-13 17:49:52 +0900

    s

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <head>
+        <meta http-equiv="Cache-Control" content="no-cache" />
+      </head>
       <body>
         <Main />
         <NextScript />
